2676cbe2d4c7b5913a48593fcc4644d3923693c1
wordenplay <67971108+wordenplay@users.noreply.github.com>
2020-07-07 13:11:57 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,31 +8,7 @@ window.onload = function () {
     $('.notice').removeClass('notice-destroy');
   }
 
-  phone();
-
-  function phone() {
-    if (phoneBack == false) {
-      $('.mobile').css('left', '30%');
-      $('.name2').addClass('name-active');
-      $('.arrow2').addClass('arrow-active');
-      setTimeout(function () {
-        $('.names').removeClass('name-active');
-        $('.arrows').removeClass('arrow-active');
-        phoneBack = true;
-        phone();
-      }, 2000);
-    } else {
-      $('.name1').addClass('name-active');
-      $('.arrow1').addClass('arrow-active');
-      $('.mobile').css('left', '10%');
-      setTimeout(function () {
-        $('.names').removeClass('name-active');
-        $('.arrows').removeClass('arrow-active');
-        phoneBack = false;
-        phone();
-      }, 2000);
-    }
-  }
+  
 
   $('.btn-true').on('click', function (e) {
     e.preventDefault();
